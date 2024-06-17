af6865248b02c3fd7644fc9733603eaf3ca0fb88
Xehiko <xexe.12@list.ru>
2024-06-18 01:23:31 +0300

    simplified "mybooks" query

--- librarydb_app/MyBooksForm.cs
+++ librarydb_app/MyBooksForm.cs
@@ -30,29 +30,15 @@ namespace librarydb_app
                                 where booking.ReaderId == readerId // фильтруем по Id читателя
                                 join book in Program.loginForm.DataContext.BookSak on booking.BookId equals book.BookId // объединяем с таблицей BookSak по полю BookId
                                 select new
-                                { // создаем анонимный объект, содержащий нужные поля для дальнейшей группировки
-                                    BookId = book.BookId,
-                                    BookTitle = book.Name,
-                                    LoanDate = booking.LoanDate,
-                                    ReturnDate = booking.ReturnDate
-                                } into books
-                                group books by new
-                                { // группируем записи по нескольким полям
-                                    books.BookId,
-                                    books.BookTitle,
-                                    books.LoanDate,
-                                    books.ReturnDate
-                                } into bookAuthors
-                                select new
                                 { // создаем новый объект для результата запроса
-                                    BookId = bookAuthors.Key.BookId, // поле BookId вытаскиваем из ключа группировки
-                                    BookTitle = bookAuthors.Key.BookTitle, // аналогично для BookTitle
+                                    BookId = booking.Book.BookId,
+                                    BookTitle = booking.Book.Name,
                                     Authors = string.Join(", ", (from author in Program.loginForm.DataContext.AuthorSak
                                                                  join bookAuthor in Program.loginForm.DataContext.BookhasauthorSak on author.AuthorId equals bookAuthor.AuthorId
-                                                                 where bookAuthor.BookId == bookAuthors.Key.BookId
+                                                                 where bookAuthor.BookId == booking.BookId
                                                                  select $"{author.LastName} {author.FirstName} {author.MiddleName}").ToList()), // объединяем всех авторов в одну строку с помощью метода Join
-                                    LoanDate = bookAuthors.Key.LoanDate, // аналогично для LoanDate
-                                    ReturnDate = bookAuthors.Key.ReturnDate // аналогично для ReturnDate
+                                    LoanDate = booking.LoanDate,
+                                    ReturnDate = booking.ReturnDate
                                 };
 
                     dataGridViewMyBooks.DataSource = query.ToList(); // устанавливаем результат запроса в качестве источника данных для dataGridViewMyBooks
